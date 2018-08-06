b4b7c81d4de6bc6eaf05e24df3ea1ba408ce3baf
guoshencheng <648772021@qq.com>
2018-08-06 10:16:08 +0800

    add some define

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,57 @@
-import axios, { AxiosRequestConfig, AxiosStatic } from 'axios';
+import axios, { AxiosStatic, AxiosRequestConfig } from 'axios';
 
-export type MethodKey = 'get' | 'post' | 'delete' | 'put' | 'patch' | 'options' | 'head';
+export type Method =
+  | 'get'
+  | 'delete'
+  | 'head'
+  | 'options'
+  | 'post'
+  | 'put'
+  | 'patch'
 
-export type Method = {
-  [key: MethodKey]: string,
-}
+const DEFAULT_METHOD = 'get';
 
-export const methods = [
-  'get', 
-  'post', 
-  'delete', 
-  'put', 
-  'patch', 
-  'options', 
-  'head'
-].reduce((pre, cur) => {
-  pre[cur] = cur;
-  return pre;
-}, {}) as Method;
+export type SearchBuilder = (data: any, config: AxiosRouterConfig) => any | undefined;
 
-export type ParamsBuilder = (data: any, api: any) => any | undefined
-
-export const searchParamsBuilder = (data: any, api): any | undefined  => {
-  const method = api.method || DEFAULT_METHOD;
-  if ([methods.get, methods.delete, methods.head, methods.options].indexOf(method.toLowerCase()) > -1) {
+export const defaultSearchBuilder = (data: any, config: AxiosRouterConfig): any | undefined => {
+  const method = config.method || DEFAULT_METHOD;
+  if (['get', 'delete', 'head', 'options'].indexOf(method.toLowerCase()) > -1) {
     return data;
   } else {
     return void 0;
   }
 }
 
-export const bodyDataBuilder = (data: any, api): any | undefined => {
-  const method = api.method || DEFAULT_METHOD;
-  if ([methods.get, methods.delete, methods.head, methods.options].indexOf(method.toLowerCase()) > -1) {
+export type DataBuilder = (data: any, config: AxiosRouterConfig) => any | undefined;
+
+export const defaultBodyDataBuilder = (data: any, config: AxiosRouterConfig): any | undefined => {
+  const method = config.method || DEFAULT_METHOD;
+  if (['get', 'delete', 'head', 'options'].indexOf(method.toLowerCase()) > -1) {
     return void 0;
   } else {
     return data;
   }
 }
 
-export interface AxiosRouterConfig {
+export interface AxiosRouterConfig extends AxiosRequestConfig {
   path: string;
+  method: Method;
+  dataBuilder?: DataBuilder;
+  searchBuilder?: SearchBuilder;
 }
+
+export interface AxiosRouterOptions {
+  router: AxiosRouterConfig
+}
+
+class AxiosRouter {
+  static Sender: AxiosStatic = axios;
+  static dataBuilder: DataBuilder = defaultBodyDataBuilder;
+  static searchBuilder: SearchBuilder = defaultSearchBuilder;
+
+  constructor({ router }: AxiosRouterOptions) {
+
+  }
+}
+
+export default AxiosRouter;
