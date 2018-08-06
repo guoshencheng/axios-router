cd1413308c45080d8548ef4acee28630fde8ca99
guoshencheng <648772021@qq.com>
2018-08-06 16:49:19 +0800

    add params builder

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import axios, { AxiosStatic, AxiosRequestConfig } from 'axios';
+import axios, { AxiosStatic, AxiosRequestConfig, AxiosPromise } from 'axios';
+import { compile } from 'path-to-regexp';
 
 export type Method =
   | 'get'
@@ -11,11 +12,13 @@ export type Method =
 
 const DEFAULT_METHOD = 'get';
 
+const isGetLike = (method) => ['get', 'delete', 'head', 'options'].indexOf(method.toLowerCase()) > -1;
+
 export type SearchBuilder = (data: any, config: AxiosRouterConfig) => any | undefined;
 
 export const defaultSearchBuilder = (data: any, config: AxiosRouterConfig): any | undefined => {
   const method = config.method || DEFAULT_METHOD;
-  if (['get', 'delete', 'head', 'options'].indexOf(method.toLowerCase()) > -1) {
+  if (isGetLike(method)) {
     return data;
   } else {
     return void 0;
@@ -34,23 +37,62 @@ export const defaultBodyDataBuilder = (data: any, config: AxiosRouterConfig): an
 }
 
 export interface AxiosRouterConfig extends AxiosRequestConfig {
-  path: string;
-  method: Method;
+  path?: string | ((data: any) => string);
+  method?: Method;
   dataBuilder?: DataBuilder;
   searchBuilder?: SearchBuilder;
 }
 
 export interface AxiosRouterOptions {
-  router: AxiosRouterConfig
+  routers: {
+    [key: string]: AxiosRouterConfig
+  }
 }
+
+export type RequestSender = (value: any, config: AxiosRouterConfig) => AxiosPromise<any>
 
 class AxiosRouter {
   static Sender: AxiosStatic = axios;
   static dataBuilder: DataBuilder = defaultBodyDataBuilder;
   static searchBuilder: SearchBuilder = defaultSearchBuilder;
 
-  constructor({ router }: AxiosRouterOptions) {
+  constructor({ routers }: AxiosRouterOptions) {
+    this.$setRouters(routers);
+  }
 
+  $setRouters(routers: {
+    [key: string]: AxiosRouterConfig
+  }) {
+    Object.keys(routers).forEach(key => {
+      Object.defineProperty(this, key, {
+        value: this.$sendRequest(routers[key]),
+        writable: false,
+      })
+    })
+  }
+
+  $sendRequest(config: AxiosRouterConfig): RequestSender {
+    return (value: any, c: AxiosRouterConfig): AxiosPromise => {
+      const method = c.method || config.method || DEFAULT_METHOD;
+      const dataBuilder = (c.dataBuilder || config.dataBuilder || AxiosRouter.dataBuilder)
+      const searchBuilder = (c.searchBuilder || config.searchBuilder || AxiosRouter.searchBuilder)
+      const path = c.path || config.path || '';
+      let params, data, url;
+      if (typeof path === 'string') {
+        url = compile(path)(value);
+      } else {
+        url = path(value);
+      }
+      params = searchBuilder(value, {
+        method,
+      });
+      data = dataBuilder(value, {
+        method,
+      });
+      return AxiosRouter.Sender(Object.assign({
+        url, params, data, method
+      }, config, c));
+    }
   }
 }
 
